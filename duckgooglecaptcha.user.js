d3429d91ee489afeb510fbeedd188d30b17b8a0d
luckynzm <31594869+luckynzm@users.noreply.github.com>
2018-12-08 22:51:03 +0100

    Create duckgooglecaptcha.user.js

---
+++ duckgooglecaptcha.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name duckgooglecaptcha
+// @namespace Violentmonkey Scripts
+// @match *://www.google.com/sorry/*
+// @author luckynzm
+// @version 0.1
+// @description Go to duckduckgo when google makes you do captcha
+// @grant none
+// ==/UserScript==
+
+var urlraw = window.location.href;
+var urldec = decodeURIComponent(urlraw);
+var trimurl = urldec.substr(urldec.indexOf("=") + 1,urldec.length);
+trimurl = trimurl.substr(trimurl.indexOf("?"),trimurl.length);
+var query = ""
+for(var i = 0; i<trimurl.length;i++) {
+  if(trimurl.charAt(i) == "&") {
+    break;
+  }
+  query += trimurl.charAt(i);
+}
+query += "&";
+location.replace("https://www.duckduckgo.com/" + query);
